5002ac195f43a85b469cbc2882ad5fed2cbaaf65
Loan Trinh <tquangtuan@orangetheoryfitness.com>
2020-07-13 12:43:24 +0700

    Updates

---
+++ precache-manifest.b734f20fd932e2c1db95eaa5da196e99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22d04da1f0b5efec0326b66ae53f010",
+    "url": "/f4u-simulator/index.html"
+  },
+  {
+    "revision": "23801449cfb43b0a6e4c",
+    "url": "/f4u-simulator/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "6453a9216fd56a62d295",
+    "url": "/f4u-simulator/static/css/main.d2e714af.chunk.css"
+  },
+  {
+    "revision": "23801449cfb43b0a6e4c",
+    "url": "/f4u-simulator/static/js/2.31bdb60a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/f4u-simulator/static/js/2.31bdb60a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6453a9216fd56a62d295",
+    "url": "/f4u-simulator/static/js/main.0b3e6baf.chunk.js"
+  },
+  {
+    "revision": "94ceb2562b37fb0d761f",
+    "url": "/f4u-simulator/static/js/runtime-main.01d13137.js"
+  }
+]);
